e6273b1db4872a04a06dddbf618e58aa4b18742f
Derth Adams <derth@me.com>
2019-06-08 14:03:38 -0700

    Add comments to main()

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/******************************************************************************
+**  Program name:  main.cpp
+**  Author:        Derth Adams
+**  Date:          June 11, 2019
+**  Description:   Main file for the "Get Into Berghain" game. Instantiates the
+**                 Game class and starts the game using Game::start().
+*******************************************************************************/
+
 #include <iostream>
 #include "Game.hpp"
 
